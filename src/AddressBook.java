70c24084056388bda46498f074d3731c675c404e
aydan <aydaneng282@gmail.com>
2025-09-15 14:46:03 -0400

    second commit

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+
 public class AddressBook {
+    private final ArrayList<BuddyInfo> buddies;
+
+    public AddressBook() {
+        this.buddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null) {
+            buddies.add(buddy);
+        }
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        buddies.remove(buddy);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
